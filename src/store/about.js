a31bda98474cb20482dc68565ea36e4f4b0c1dfd
남상우 <hotdari90@gmail.com>
2021-07-17 19:24:07 +0900

    create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'HEROPY',
+    email: 'hotdari@naver.com',
+    blog: 'velog.ol',
+    phone: '+82-10-1234-5678',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
