400a517e0b05a8431d623ea0b4c0e81bfd98d943
남상우 <hotdari90@gmail.com>
2021-07-18 17:02:37 +0900

    fix functions

--- functions/hello.js
+++ functions/hello.js
@@ -2,9 +2,9 @@ export.handler = async function (event, context) {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            name: 'HOTDARI',
+            name: "HOTDARI",
             age: 33,
-            email: 'hotdari90@naver.com'
+            email: "hotdari90@naver.com"
         })
     }
 }
